44de1829a49cad32ddbec50f8774ae09e1b84228
zhangqi <1564171310@qq.com>
2017-12-06 04:28:43 +0800

    First Commit For TTs

---
+++ TTS 01/Assets/Scripts/Tts/MicrophoneTest.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneTest : MonoBehaviour {
+
+    public enum AudioRecordResultState { Success, NoMicrophone, TooShort }
+    [SerializeField] private int maxClipLength = 300;
+    [HideInInspector] public bool isRecording = false;
+
+
+    private AudioClip recordedClip;
+    private int _sampleWindow = 128;
+    private float recordTimer = 0.0f;
+
+    private void Update()
+    {
+        if (isRecording)
+        {
+            recordTimer += Time.deltaTime;
+
+        }
+    }
+
+
+    /// <summary>
+    /// 开始录制
+    /// </summary>
+    public AudioRecordResultState StartRecord()
+    {
+        if (Microphone.devices.Length <= 0)
+            return AudioRecordResultState.NoMicrophone;
+
+
+        recordTimer = 0;
+        recordedClip = Microphone.Start(null, false, maxClipLength, 16000);
+
+        isRecording = true;
+        Debug.Log("start record-------------");
+        return AudioRecordResultState.Success;
+    }
+
+
+    /// <summary>
+    /// 获取麦克风音量
+    /// </summary>
+    /// <returns></returns>
+    public float GetLevelMax()
+    {
+        float levelMax = 0;
+        float[] waveData = new float[_sampleWindow];
+        int micPosition = Microphone.GetPosition(null) - (_sampleWindow + 1); // null means the first microphone
+        if (micPosition < 0) return 0;
+        recordedClip.GetData(waveData, micPosition);
+
+        // Getting a peak on the last 128 samples
+        for (int i = 0; i < _sampleWindow; i++)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        return levelMax;
+    }
+
+
+
+    /// <summary>
+    /// 停止录制
+    /// </summary>
+    /// <returns>返回音频保存路径</returns>
+    public AudioRecordResultState StopRecord(out string filePath)
+    {
+        Debug.Log("stop record---------------");
+
+        //Capture the current clip data
+        isRecording = false;
+        if (recordTimer < 0.5f)
+        {
+            filePath = null;
+            return AudioRecordResultState.TooShort;
+        }
+
+        int position = Microphone.GetPosition(null);
+        var soundData = new float[recordedClip.samples * recordedClip.channels];
+        recordedClip.GetData(soundData, 0);
+
+        //Create shortened array for the data that was used for recording
+        var newData = new float[position * recordedClip.channels];
+
+
+        //Copy the used samples to a new array
+        for (int i = 0; i < newData.Length; i++)
+        {
+            newData[i] = soundData[i];
+        }
+
+        //One does not simply shorten an AudioClip,
+        //    so we make a new one with the appropriate length
+        recordedClip = AudioClip.Create(recordedClip.name,
+                                        position,
+                                        recordedClip.channels,
+                                        recordedClip.frequency,
+                                        false);
+
+        recordedClip.SetData(newData, 0);        //Give it the data from the old clip
+
+        //Replace the old clip
+        Microphone.End(null);
+
+        //save to disk
+        string recordedAudioPath;
+        byte[] data = WavUtility.FromAudioClip(recordedClip, out recordedAudioPath, true);
+        filePath = recordedAudioPath;
+
+        return AudioRecordResultState.Success;
+    }
+}
---
+++ TTS 01/Assets/Scripts/Tts/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program : MonoBehaviour
+    {
+        void Start()
+        {
+            #region  tts  文字--》语音
+            String url = "http://nlsapi.aliyun.com/speak?";
+            //String tts_text = "薄雾浓云愁永昼。瑞脑消金兽。佳节又重阳，玉枕纱厨，半夜凉初透。东篱把酒黄昏后。有暗香盈袖。莫道不消魂，帘卷西风，人比黄花瘦。";
+            String tts_text = "32号谁啊，我怎么没见过？";
+            //请使用https://ak-console.aliyun.com/ 页面获取的Access 信息
+            //请提前开通智能语音服务(https://data.aliyun.com/product/nls)
+            String ak_id = "LTAIr8I8A5HNZvdt";
+            String ak_secret = "rBSsm6hewfAcSH6oeA6VXF6PCAACGD";
+            //String ak_id = "LTAILNHYi9oS9tmi";
+            //String ak_secret = "hZH1gI8FYIeOPc7rFxtQl8geg7aaGW";
+
+            //设置TTS的参数,详细参数说明详见文档部分2.1 参数配置
+            Program ttsDemo = new Program();
+            TtsRequest ttsRequest = new TtsRequest();
+            ttsRequest.setEncodeType("wav");
+            ttsRequest.setVoiceName("xiaoyun");
+            ttsRequest.setVolume(50);
+            ttsRequest.setSampleRate(16000);
+            ttsRequest.setSpeechRate(0);
+            ttsRequest.setPitchRate(0);
+            ttsRequest.setTtsNus(1);
+            ttsRequest.setBackgroundMusicId(0);
+            ttsRequest.setBackgroundMusicOffset(0);
+            ttsRequest.setBackgroundMusicVolume(0);
+
+            String ttsUrlSpeak = url + "encode_type=" + ttsRequest.getEncodeType()
+                     + "&voice_name=" + ttsRequest.getVoiceName()
+                     + "&volume=" + ttsRequest.getVolume()
+                     + "&sample_rate=" + ttsRequest.getSampleRate()
+                     + "&speech_rate=" + ttsRequest.getSpeechRate()
+                     + "&pitch_rate=" + ttsRequest.getPitchRate()
+                     + "&tts_nus=" + ttsRequest.getTtsNus()
+                    + "&background_music_id=" + ttsRequest.getBackgroundMusicId()
+                    + "&background_music_offset=" + ttsRequest.getBackgroundMusicOffset()
+                    + "&background_music_volume=" + ttsRequest.getBackgroundMusicVolume();
+
+
+            string fileName = System.Guid.NewGuid().ToString("N");
+            //HttpResponse response = HttpUtil.sendTtsPost(tts_text, ttsRequest.getEncodeType(), fileName, ttsUrlSpeak, ak_id, ak_secret);
+            #endregion
+
+            #region tts 语音--》文字
+            //String urls = "http://nlsapi.aliyun.com/recognize?";
+            ////使用对应的ASR模型 详情见文档部分2
+            //String model = "chat";
+            //urls = urls + "model=" + model;
+            //FileStream fs = new FileStream(@"D:\1.wav", FileMode.Open, FileAccess.Read);
+            //byte[] audioBytes = new byte[fs.Length];
+            //fs.Read(audioBytes, 0, (int)fs.Length);
+
+            //HttpResponse responseRecognize = HttpUtil.sendAsrPost(audioBytes, "pcm", 16000, urls, ak_id, ak_secret);
+
+            //fs.Close();
+            #endregion
+
+
+            ////智能对话
+            //String urlStr = "https://nlsapi.aliyun.com/manage/qas?action=single:prepub:qa";
+            ////String urlStr = "https://nlsapi.aliyun.com/manage/qas?action=projects:list";
+            //String urlStrTest = "https://nlsapi.aliyun.com/manage/qas";
+
+            //String bdStr = "{\"projectId" + "\"" + ":" + 4649 + "," + "\"question" + "\"" + ":" + "\"" + "请问勒布朗詹姆斯最近的技术统计怎么样？" + "\"}";
+
+            ////String bdStr = "{\"offset" + "\"" + ":" + 0+ "," + "\"pageSize" + "\"" + ":" + 1 +"}";
+
+            //String bdStrTest = "{\"version" + "\"" + ":" + "\"" + "2.0" + "\"" + "," + "\"app_key" + "\"" + ":" + "\"" + "nui-d9lLBVpmR0dl" + "\"" + "," + "\"question" + "\"" + ":" + "\"" + "请问勒布朗詹姆斯最近的技术统计怎么样？" + "\"}";
+
+            //String responseStrTest = HttpProxy.sendRequest(urlStrTest, bdStrTest, ak_id, ak_secret);
+            //Debug.Log(responseStrTest);
+
+            //String responseStr = HttpProxy.sendRequest(urlStr, bdStr, ak_id, ak_secret);
+            // Debug.Log(responseStr);
+
+            //语音采集
+            //SoundRecord recorder = new SoundRecord();
+            ////开始录音
+            //string wavfile = null;
+            //wavfile = "test.wav";
+            //recorder.SetFileName(wavfile);
+            //recorder.RecStart();
+            ////结束
+            //recorder.RecStop();
+            //recorder = null;
+
+        }
+
+    }
+}
